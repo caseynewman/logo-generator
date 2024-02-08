e57c204974ff609db665dec61162f21be65ee083
Yogi Spice <caseynnewman@yahoo.com>
2024-02-08 09:23:16 -0500

    made createText and createShape functions

--- index.js
+++ index.js
@@ -2,35 +2,47 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const shapes = require('./lib/shapes');
 
-// class CLI {
-//     constructor() {
-        // this.textInput = '';
-        // this.shape = '';
-//     }
-// }
+class CLI {
+    constructor() {
+        this.textInput = '';
+        this.logoShape = '';
+    }
+
+    render() {
+        return `<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg"></svg>`
+    }
+
+    createText(logoText, textColor) {
+        this.textInput = `<text x="150" y="125" font-size="60" fill="${textColor}">${logoText}</text>`
+    }
+
+    createShape(shape) {
+        this.logoShape = shape.render();
+    }
+}
 
 inquirer
     .prompt([
         {
             type: 'input',
+            name: 'logoText',
             message: 'Enter text for your logo (must be no longer than 3 characters).',
-            name: 'logo-text',
         },
         {
             type: 'input',
+            name: 'textColor',
             message: 'Enter a font color or HEX code',
-            name: 'text-color',
         },
         {
             type: 'list',
-            message: 'Choose your logo shape',
             name: 'shape',
+            message: 'Choose your logo shape',
             choices: ['Triangle', 'Circle', 'Square'],
         },
         {
             type: 'input',
+            name: 'shapeColor',
             message: 'Enter a color or HEX code for your shape',
-            name: 'shape-color',
         }
     ])
 
