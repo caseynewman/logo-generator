7817e6b30401af11bef92ca06d8f660b6ec43a19
Yogi Spice <caseynnewman@yahoo.com>
2024-02-08 00:19:24 -0500

    removed cli file and prompts are now running correctly

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-const CLI = require('./lib/cli');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const shapes = require('./lib/shapes');
 
-new CLI().run();
+// class CLI {
+//     constructor() {
+        // this.textInput = '';
+        // this.shape = '';
+//     }
+// }
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Enter text for your logo (must be no longer than 3 characters).',
+            name: 'logo-text',
+        },
+        {
+            type: 'input',
+            message: 'Enter your text color',
+            name: 'text-color',
+        },
+        {
+            type: 'list',
+            message: 'Choose your logo shape',
+            name: 'shape',
+            choices: ['Triangle', 'Circle', 'Square'],
+        },
+        {
+            type: 'input',
+            message: 'Enter a color for your shape',
+            name: 'shape-color',
+        }
+    ])
+
+    .then((input) => {
+        const filename = `logo.svg`;
+
+        fs.writeFile(filename, JSON.stringify(input, null, '\t'), (err) =>
+        err ? console.log(err) : console.log('Success!')
+      );
+    });
