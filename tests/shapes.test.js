f5dfa62a9e2df34a92ce4084590d4eb27e732617
Yogi Spice <caseynnewman@yahoo.com>
2024-02-08 22:55:23 -0500

    added tests for Square and Circle

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -5,7 +5,21 @@ describe('Shapes', () => {
   describe('render Triangle', () => {
     it('should return a triangle', () => {
         const shape = new Triangle('cnn', 'pink', 'blue');
-        expect(shape.render()).toEqual('<polygon points="200,10 250,190 150,190" fill="blue"/>');
+        expect(shape.render()).toEqual('<polygon points="185 5, 270 135, 100 135" fill="blue"/>');
+    })
+  })
+
+  describe('render Circle', () => {
+    it('should return a circle', () => {
+        const shape = new Circle('cnn', 'pink', 'orange');
+        expect(shape.render()).toEqual('<circle cx="185" cy="45%" r="85" fill="orange"/>');
+    })
+  })
+
+  describe('render Square', () => {
+    it('should return a square', () => {
+        const shape = new Square('cnn', 'pink', 'green');
+        expect(shape.render()).toEqual('<rect x="110" y="10" width="150" height="150" fill="green"/>');
     })
   })
 })
